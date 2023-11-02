d3dc28fbf839041c4d4a458f50477d95f4ab3537
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-11-02 16:36:20 -0400

    Add new policy to IAM managers group
    Also, handle renamed policy.

--- groups.tf
+++ groups.tf
@@ -5,10 +5,20 @@ resource "aws_iam_group" "iam_user_group_managers" {
   name = var.iam_managers_group_name
 }
 
-# Attach the policy that allows assumption of the IAM user/group management role.
-resource "aws_iam_group_policy_attachment" "iam_user_group_managers" {
+# Attach the policy that allows group members to assume roles needed in order
+# to manage IAM users and groups.
+resource "aws_iam_group_policy_attachment" "iam_manager_roles" {
   provider = aws.users
 
   group      = aws_iam_group.iam_user_group_managers.name
-  policy_arn = aws_iam_policy.iam_managers.arn
+  policy_arn = aws_iam_policy.iam_manager_roles.arn
+}
+
+# Attach the policy that allows group members to manage login profiles and MFA
+# devices for IAM users.
+resource "aws_iam_group_policy_attachment" "iam_manager_login_mfa" {
+  provider = aws.users
+
+  group      = aws_iam_group.iam_user_group_managers.name
+  policy_arn = aws_iam_policy.iam_manager_login_mfa.arn
 }
