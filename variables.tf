f25511453567f3f546d4615415ac47a72b8b8d72
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-11-02 10:46:34 -0400

    Define variables

--- variables.tf
+++ variables.tf
@@ -4,9 +4,9 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "subnet_id" {
-  type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)."
+variable "users" {
+  type        = list(string)
+  description = "A list containing the usernames of users that exist in the Users account who are allowed to manage IAM users and groups.  Example: [ \"firstname1.lastname1\", \"firstname2.lastname2\" ]."
 }
 
 # ------------------------------------------------------------------------------
@@ -14,20 +14,33 @@ variable "subnet_id" {
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
-}
-
-variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default     = "a"
-}
 
 variable "aws_region" {
-  type        = string
-  description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  type        = string
+}
+
+variable "iam_managers_group_name" {
+  default     = "iam_user_group_managers"
+  description = "The name of the IAM group whose members are allowed to manage IAM users and groups."
+  type        = string
+}
+
+variable "iam_managers_policy_description" {
+  default     = "Allows the IAM managers group to assume all roles needed in order to manage IAM users and groups."
+  description = "The description to associate with the IAM policy in the Users account that allows the IAM managers group to assume all roles needed in order to manage IAM users and groups."
+  type        = string
+}
+
+variable "iam_managers_policy_name" {
+  default     = "AssumeRolesToManageIAMUsersAndGroups"
+  description = "The name of the IAM policy in the Users account that allows the IAM managers group to assume all roles needed in order to manage IAM users and groups."
+  type        = string
+}
+
+variable "tags" {
+  default     = {}
+  description = "Tags to apply to all AWS resources created."
+  type        = map(string)
 }
