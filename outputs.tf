fd0f403174d8f0ac6cc2259ad7a173df4fa3474e
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-11-02 10:50:26 -0400

    Replace skeleton outputs with real outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,9 @@
-output "arn" {
-  value       = aws_instance.example.arn
-  description = "The EC2 instance ARN."
+output "iam_managers_group" {
+  value       = aws_iam_group.iam_user_group_managers
+  description = "The IAM group whose members are allowed to manage IAM users and groups."
 }
 
-output "availability_zone" {
-  value       = aws_instance.example.availability_zone
-  description = "The AZ where the EC2 instance is deployed."
-}
-
-output "id" {
-  value       = aws_instance.example.id
-  description = "The EC2 instance ID."
-}
-
-output "private_ip" {
-  value       = aws_instance.example.private_ip
-  description = "The private IP of the EC2 instance."
-}
-
-output "subnet_id" {
-  value       = aws_instance.example.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed."
+output "iam_managers_policy" {
+  value       = aws_iam_policy.iam_managers
+  description = "The IAM policy in the Users account that allows the IAM managers group to assume all roles needed in order to manage IAM users and groups."
 }
